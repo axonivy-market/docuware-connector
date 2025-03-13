fc7c03eb69aceeb097213420c1530383865a29d8
nqhoan-axonivy <hoan.nguyen01@axonactive.com>
2025-03-13 17:06:17 +0700

    Clean up code

--- docuware-connector/src/com/axonivy/connector/docuware/connector/DocuWareEndpointConfiguration.java
+++ docuware-connector/src/com/axonivy/connector/docuware/connector/DocuWareEndpointConfiguration.java
@@ -8,30 +8,8 @@ public class DocuWareEndpointConfiguration {
   private String fileCabinetId;
   private List<String> fileCabinetIds;
   private String storeDialogId;
-  private List<String> storeDialogIds;
-  private String organizationId;
-  private List<String> organizationIds;
 
   public DocuWareEndpointConfiguration() { }
-
-  public DocuWareEndpointConfiguration(String fileCabinetId, String storeDialogId, String organizationId) {
-    this.fileCabinetId = fileCabinetId;
-    this.fileCabinetIds = List.of(fileCabinetId);
-    this.storeDialogId = storeDialogId;
-    this.storeDialogIds = List.of(storeDialogId);
-    this.organizationId = organizationId;
-    this.organizationIds = List.of(organizationId);
-  }
-
-  public DocuWareEndpointConfiguration(String fileCabinetId, List<String> fileCabinetIds, String storeDialogId,
-      List<String> storeDialogIds, String organizationId, List<String> organizationIds) {
-    this.fileCabinetId = fileCabinetId;
-    this.fileCabinetIds = fileCabinetIds;
-    this.storeDialogId = storeDialogId;
-    this.storeDialogIds = storeDialogIds;
-    this.organizationId = organizationId;
-    this.organizationIds = organizationIds;
-  }
 
   public String getFileCabinetId() {
     return fileCabinetId;
@@ -57,35 +35,9 @@ public class DocuWareEndpointConfiguration {
     this.storeDialogId = storeDialogId;
   }
 
-  public List<String> getStoreDialogIds() {
-    return storeDialogIds;
-  }
-
-  public void setStoreDialogIds(List<String> storeDialogIds) {
-    this.storeDialogIds = storeDialogIds;
-  }
-
-  public String getOrganizationId() {
-    return organizationId;
-  }
-
-  public void setOrganizationId(String organizationId) {
-    this.organizationId = organizationId;
-  }
-
-  public List<String> getOrganizationIds() {
-    return organizationIds;
-  }
-
-  public void setOrganizationIds(List<String> organizationIds) {
-    this.organizationIds = organizationIds;
-  }
-
   @Override
   public String toString() {
-    return String.format("filecabinet: %s / storeDialogId: %s / organization: %s",
-        Arrays.asList(fileCabinetId, fileCabinetIds),
-        Arrays.asList(storeDialogId, storeDialogIds),
-        Arrays.asList(organizationId, organizationIds));
+    return String.format("filecabinet: %s / storeDialogId: %s", Arrays.asList(fileCabinetId, fileCabinetIds),
+        storeDialogId);
   }
 }
--- docuware-connector/src/com/axonivy/connector/docuware/connector/converter/PojoConverter.java
+++ docuware-connector/src/com/axonivy/connector/docuware/connector/converter/PojoConverter.java
@@ -9,12 +9,13 @@ import javax.faces.convert.Converter;
 import javax.faces.convert.ConverterException;
 import javax.faces.convert.FacesConverter;
 
+import static org.apache.commons.lang3.StringUtils.EMPTY;
+
 @FacesConverter("pojoConverter")
 public class PojoConverter implements Converter {
   private static final String UNIQUE_CONVERTER_IDENTIFIER = PojoConverter.class.getName();
   private static final String KEY_DELIMITER = ":::";
-  private static final String MAP_KEY_TEMPLATE = UNIQUE_CONVERTER_IDENTIFIER + KEY_DELIMITER + "%s" + KEY_DELIMITER
-      + "%s";
+  private static final String MAP_KEY_TEMPLATE = UNIQUE_CONVERTER_IDENTIFIER + KEY_DELIMITER + "%s" + KEY_DELIMITER + "%s";
 
   @Override
   public String getAsString(FacesContext context, UIComponent component, Object item) throws ConverterException {
@@ -26,13 +27,12 @@ public class PojoConverter implements Converter {
 
       return identityHashCode;
     }
-    return "";
+    return EMPTY;
   }
 
   @Override
   public Object getAsObject(FacesContext context, UIComponent component, String selectedvalue) {
     if (selectedvalue != null && selectedvalue.length() > 0) {
-
       String mapKey = String.format(MAP_KEY_TEMPLATE, component.getId(), selectedvalue);
       Map<String, Object> viewMap = getViewMap(context);
 
@@ -42,7 +42,7 @@ public class PojoConverter implements Converter {
   }
 
   private boolean isEmptyString(Object item) {
-    return String.class.isAssignableFrom(item.getClass()) && "".equals(item);
+    return String.class.isAssignableFrom(item.getClass()) && EMPTY.equals(item);
   }
 
   private Map<String, Object> getViewMap(FacesContext context) {
