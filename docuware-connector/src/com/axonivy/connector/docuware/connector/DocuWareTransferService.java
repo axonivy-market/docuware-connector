2bea6f523ba24750d1ab5c1f4fcd5c42c112ef25
anh.phamtu <anh.phamtu@axonactive.com>
2026-05-20 14:12:23 +0700

    MARP-4319 TransferService.moveDocument

---
+++ docuware-connector/src/com/axonivy/connector/docuware/connector/DocuWareTransferService.java
@@ -0,0 +1,43 @@
+package com.axonivy.connector.docuware.connector;
+
+import com.docuware.dev.schema._public.services.platform.DocumentsQueryResult;
+import com.docuware.dev.schema._public.services.platform.FileCabinetTransferInfo;
+import com.fasterxml.jackson.core.JsonProcessingException;
+
+import ch.ivyteam.ivy.bpm.error.BpmError;
+
+/**
+ * Service class for document transfer operator
+ */
+public class DocuWareTransferService {
+	protected static final DocuWareTransferService INSTANCE = new DocuWareTransferService();
+	
+	public static DocuWareTransferService get() {
+		return INSTANCE;
+	}
+
+	/**
+	 * move document from sourceFileCabinet 
+	 * @param sourceFileCabinetId
+	 * @param sourceDocumentId
+	 * @return
+	 * @throws JsonProcessingException
+	 */
+	public String moveDocumentRequestBody(String sourceFileCabinetId, String sourceDocumentId) throws JsonProcessingException {
+		int sourceDocId = 0;
+		try {
+			sourceDocId = Integer.parseInt(sourceDocumentId);
+		} catch (Exception e) {
+			BpmError
+			.create(DocuWareService.DOCUWARE_ERROR + "invalidSourceDocumentId")
+			.withMessage("Could not parse sourceDocumentId '%s'".formatted(sourceDocumentId))
+			.throwError();
+		}
+		FileCabinetTransferInfo request = new FileCabinetTransferInfo();
+		request.setSourceFileCabinetId(sourceFileCabinetId);
+		request.getSourceDocId().add(sourceDocId);
+		request.setFillIntellix(false);
+		request.setKeepSource(false);
+		return JsonUtils.getObjectMapper().writeValueAsString(request);
+	}
+}
